a1ee670f6d53289cd28f92d04732638023eaf029
Fitamant Quentin <quentinfitamantp@gmail.com>
2022-07-19 16:32:26 +0200

    Fin de journée 19/07

--- src/main/java/org/sid/web/ProductController.java
+++ src/main/java/org/sid/web/ProductController.java
@@ -1,28 +1,30 @@
 package org.sid.web;
 
-import java.util.List;
-
-import org.hibernate.criterion.Order;
 import org.sid.dao.ProductRepository;
 import org.sid.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
 	@Autowired
 	private ProductRepository productRepository;
+	
 	@RequestMapping(value="/index")
-	public String index(Model model) {
-		Pageable pageable = PageRequest.of(0, 10,  Sort.by((List<org.springframework.data.domain.Sort.Order>) Order.desc("id")));
+	public String index(Model model, 
+			@RequestParam(name="page", defaultValue="0")int p, 
+			@RequestParam(name="size", defaultValue="5")int s) {
+		Pageable pageable = PageRequest.of(p, s);
 		Page<Product> pageProducts = productRepository.findAll(pageable);
 		model.addAttribute("listProducts", pageProducts.getContent());
+		int[] pages = new int[pageProducts.getTotalPages()];
+		model.addAttribute("pages", pages);
 		return "product";
 	}
 }
